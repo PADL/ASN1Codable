2596d623ed1664d198a80975cfc522280480380a
Luke Howard <lukeh@padl.com>
2022-11-07 10:01:27 +1100

    reformat

--- Sources/ASN1CodingKit/Encoder/SingleValueEncodingContainer.swift
+++ Sources/ASN1CodingKit/Encoder/SingleValueEncodingContainer.swift
@@ -93,8 +93,7 @@ extension ASN1EncoderImpl.SingleValueContainer: SingleValueEncodingContainer {
     
     private func encodeFixedWithInteger<T>(_ value: T) throws where T: FixedWidthInteger {
         try self.mappingASN1Error(value) {
-            let object = T.isSigned ?
-            try Int(value).asn1encode(tag: nil) : try UInt(value).asn1encode(tag: nil)
+            let object = T.isSigned ? try Int(value).asn1encode(tag: nil) : try UInt(value).asn1encode(tag: nil)
             self.object = object
         }
     }
