544975a93c9eff3b735275b6d5bb66485143cda7
Luke Howard <lukeh@padl.com>
2022-11-17 14:09:03 +1100

    add --output argument to asn1json2swift

--- Sources/CLI/asn1json2swift/TranslateCommand.swift
+++ Sources/CLI/asn1json2swift/TranslateCommand.swift
@@ -21,6 +21,7 @@ import HeimASN1Translator
 struct TranslateCommand: CommandProtocol {
     enum Error: Swift.Error {
         case failedToOpenInputStream
+        case failedToOpenOutputStream
         case translationError(Swift.Error)
     }
 
@@ -28,7 +29,7 @@ struct TranslateCommand: CommandProtocol {
     let function: String = "Translate asn1compile JSON output to Swift"
 
     func run(_ options: Options) -> Result<(), Error> {
-        let file = URL(fileURLWithPath: (options.file as NSString).expandingTildeInPath)
+        let file = URL(fileURLWithPath: (options.input as NSString).expandingTildeInPath)
 
         guard let inputStream = InputStream(url: file) else {
             return .failure(.failedToOpenInputStream)
@@ -46,14 +47,27 @@ struct TranslateCommand: CommandProtocol {
             typeMapDictionary = nil
         }
         
-        var outputStream: OutputStream = OutputStream(toMemory: ())
+        var outputStream: OutputStream
+        
+        if let output = options.output {
+            guard let o = OutputStream(toFileAtPath: output, append: false) else {
+                return .failure(.failedToOpenOutputStream)
+            }
+            outputStream = o
+        } else {
+            outputStream = OutputStream(toMemory: ())
+        }
+        
         let translator = HeimASN1Translator(inputStream: inputStream, outputStream: &outputStream, typeMaps: typeMapDictionary)
 
         do {
             try translator.translate()
-            let data = outputStream.property(forKey: Stream.PropertyKey.dataWrittenToMemoryStreamKey) as! Data
-            let string = String(data: data, encoding: .utf8)!
-            print("\(string)")
+            
+            if options.output == nil {
+                let data = outputStream.property(forKey: Stream.PropertyKey.dataWrittenToMemoryStreamKey) as! Data
+                let string = String(data: data, encoding: .utf8)!
+                print("\(string)")
+            }
         } catch {
             return .failure(.translationError(error))
         }
@@ -62,19 +76,20 @@ struct TranslateCommand: CommandProtocol {
     }
 
     struct Options: OptionsProtocol {
-        let file: String
+        let input: String
+        let output: String?
         let typeMaps: [String]?
 
-        static func create(_ file: String) -> (_ typeMaps: [String]?) -> Options {
-            return { (_ typeMaps: [String]?) in
-                return Options(file: file, typeMaps: typeMaps)
-            }
+        static func create(_ input: String) -> (_ output: String?) -> (_ typeMaps: [String]?) -> Options {
+            return { (_ output: String?) in { (_ typeMaps: [String]?) in
+                return Options(input: input, output: output, typeMaps: typeMaps)
+            }}
         }
         
         static func evaluate(_ m: CommandMode) -> Result<Options, CommandantError<Error>> {
-            //swiftlint:disable:previous identifier_name
             return create
-                    <*> m <| Option(key: "file", defaultValue: "", usage: "path to JSON output from asn1compile")
+                    <*> m <| Option<String>(key: "input", defaultValue: "", usage: "path to JSON output from asn1_compile")
+                    <*> m <| Option<String?>(key: "output", defaultValue: nil, usage: "path to Swift output file")
                     <*> m <| Option<[String]?>(key: "map-type", defaultValue: nil, usage: "replace ASN.1 type with user-defined Swift type")
         }
     }
