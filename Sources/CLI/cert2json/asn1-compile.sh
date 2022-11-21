132436ef6e9015bd50da6b77398f7f5e78a85548
Luke Howard <lukeh@padl.com>
2022-11-21 12:06:13 +1100

    use installed asn1_compile

--- Sources/CLI/cert2json/asn1-compile.sh
+++ Sources/CLI/cert2json/asn1-compile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-../../../../../heimdal/lib/asn1/asn1_compile --one-code-file --option-file=rfc2459.opt rfc2459.asn1
+/usr/local/heimdal/bin/asn1_compile --one-code-file --option-file=rfc2459.opt rfc2459.asn1
