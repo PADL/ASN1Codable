0061ded3fe6cdb14a8f7e7dfb17668c56a45689c
Luke Howard <lukeh@padl.com>
2022-11-23 13:17:17 +1100

    cleanup

--- Sources/ASN1Codable/Common/ASN1Object+Sorting.swift
+++ Sources/ASN1Codable/Common/ASN1Object+Sorting.swift
@@ -33,28 +33,30 @@ extension ASN1Object {
             return self
         }
 
-        let sorted = items.sorted(by: { lhs, rhs in
-            do {
-                let lhs_serialized = try lhs.serialize()
-                let rhs_serialized = try rhs.serialize()
-                
-                if lhs_serialized.count == rhs_serialized.count {
-                    let cmp = lhs_serialized.withUnsafeBytes { lhs_bytes in
-                        rhs_serialized.withUnsafeBytes { rhs_bytes in
-                            return memcmp(lhs_bytes.bindMemory(to: UInt8.self).baseAddress,
-                                          rhs_bytes.bindMemory(to: UInt8.self).baseAddress,
-                                          lhs_serialized.count)
-                        }
-                    }
-                    return cmp > 0
-                } else {
-                    return lhs_serialized.count < rhs_serialized.count
-                }
-            } catch {
-                return false
-            }
-        })
+        let sorted = items.sorted(by: Self.sort)
 
         return ASN1Kit.create(tag: self.tag, data: .constructed(sorted))
+    }
+    
+    private static func sort(_ lhs: ASN1Object, _ rhs: ASN1Object) -> Bool {
+        do {
+            let lhs_serialized = try lhs.serialize()
+            let rhs_serialized = try rhs.serialize()
+            
+            if lhs_serialized.count == rhs_serialized.count {
+                let cmp = lhs_serialized.withUnsafeBytes { lhs_bytes in
+                    rhs_serialized.withUnsafeBytes { rhs_bytes in
+                        return memcmp(lhs_bytes.bindMemory(to: UInt8.self).baseAddress,
+                                      rhs_bytes.bindMemory(to: UInt8.self).baseAddress,
+                                      lhs_serialized.count)
+                    }
+                }
+                return cmp > 0
+            } else {
+                return lhs_serialized.count < rhs_serialized.count
+            }
+        } catch {
+            return false
+        }
     }
 }
