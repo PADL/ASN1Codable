692cb54ab7bb893b96dc155c68ad5f11d4c5a18b
Luke Howard <lukeh@padl.com>
2022-11-26 20:45:12 +1100

    make ASN1PreserveBinary a class-only protocol

--- Sources/ASN1Codable/Protocols/ASN1PreserveBinary.swift
+++ Sources/ASN1Codable/Protocols/ASN1PreserveBinary.swift
@@ -16,7 +16,7 @@
 
 import Foundation
 
-public protocol ASN1PreserveBinary {
+public protocol ASN1PreserveBinary: class {
     // swiftlint:disable identifier_name
     var _save: Data? { get set }
 }
