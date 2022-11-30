e84d267ac04ea07a981e1d8982794e7191e0a609
Luke Howard <lukeh@padl.com>
2022-11-30 12:11:54 +1100

    use PR#71 of Azoy/Echo to fix release build

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/PADL/ASN1Kit", .branch("lhoward/pr")),
-        .package(url: "https://github.com/Azoy/Echo", .branch("main")),
+        .package(url: "https://github.com/ole/Echo", .branch("fix-release-build")),
         .package(url: "https://github.com/mkrd/Swift-BigInt", .branch("master")),
         .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.7"),
         .package(url: "https://github.com/Carthage/Commandant", from: "0.9.0")
