be9e8228ea37365a43a56cfe153b7c96b81a1474
Luke Howard <lukeh@padl.com>
2022-12-05 19:00:06 +1100

    disable AUTOMATIC TAGS for collection types

--- Tests/ASN1CodableTests/ASN1CodableTests+Constructed.swift
+++ Tests/ASN1CodableTests/ASN1CodableTests+Constructed.swift
@@ -498,31 +498,25 @@ extension ASN1CodableTests {
     }
 
     func test_SEQUENCE_Automatic() {
-        struct Key: Codable, Equatable, Hashable {
-            var value: UInt
-
-            init(_ value: UInt) {
-                self.value = value
-            }
-        }
-
-        let dictionary = [Key(1234): "Europe", Key(5555): "Sweden"]
+        let dictionary = [1234: "Europe", 5555: "Sweden"]
         let r_and_d = Division.RAndD(labID: 48, currentProject: Data([0x44, 0x58, 0x2D, 0x37]))
         let pr = PersonnelRecord(name: "Luke", location: 2, age: 102)
 
         struct SomeType: Codable, Equatable {
-            let dictionary: [Key: String]
+            let set: Set<String>
+            let dictionary: [Int: String]
             let division: Division
             let personnelRecord: PersonnelRecord
         }
 
-        let st = SomeType(dictionary: dictionary,
+        let st = SomeType(set: Set<String>(arrayLiteral: "a", "b"),
+                          dictionary: dictionary,
                           division: Division.r_and_d(r_and_d),
                           personnelRecord: pr)
 
         self.test_encodeDecode(st,
-                               encodedAs: try! Data(hex: "3043A020300E3004020204D20C064575726F7065300E3004020215B30C0653776564" +
-                                   "656EA10BA109800130810444582D37A212A0060C044C756B65A103020102A203020166"),
+                               encodedAs: try! Data(hex: "3047A0060C01610C0162A11C300C800204D281064575726F7065300C800215B3810653776564656EA20BA10980" +
+                                   "0130810444582D37A312A0060C044C756B65A103020102A203020166"),
                                taggingEnvironment: .automatic)
     }
 }
